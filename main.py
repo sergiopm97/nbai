c4bdb2235f34881a5105cc5d13b63b1fe0cc0932
Sergio <sergio.pm.97@protonmail.com>
2022-11-09 19:11:24 +0100

    Extract selected columns
    Extraction of the columns that have been used to train the winner model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from classes import Nbai
 import configparser
+import ast
 
 
 config = configparser.ConfigParser()
@@ -23,4 +24,8 @@ def get_predictions_by_date(date: str) -> dict:
 
     nbai.matches_by_date = nbai.get_matches_by_date(
         nbai.nba_matches_df, config["COLUMNS"]["date"], date
+    )
+
+    nbai.selected_columns_df = nbai.select_columns(
+        nbai.matches_by_date, ast.literal_eval(config["COLUMNS"]["features"])
     )
